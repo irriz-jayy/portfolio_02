dfa0ff31d2564248931364d8646232c732ab049c
irriz-jayy <jayfelix789@gmail.com>
2023-02-26 22:50:33 +0300

    Added first logo

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -2,12 +2,19 @@ const Connect = () => {
   return (
     <div className="connect-container">
       <h1 className="connect-heading">Connect with me</h1>
-      <li>
-        <a href="#">Home</a>
-        <a href="#">Home</a>
-        <a href="#">Home</a>
-        <a href="#">Home</a>
-      </li>
+      <p className="connect-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo molestiae
+        neque impedit at dicta nam recusandae error eos sint similique quis
+        corporis perspiciatis accusamus, ut dolores! Sit delectus laborum
+        repellendus.
+      </p>
+      <img
+        src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-256.png"
+        alt="logo"
+        width="50"
+        height="50"
+        className="logo"
+      />
     </div>
   );
 };
