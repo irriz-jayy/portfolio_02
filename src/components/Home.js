fafa0e4a586d63ac7b87f867c25748e7b64fe8ea
irriz-jayy <jayfelix789@gmail.com>
2023-02-27 23:53:57 +0300

    Minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
         <div className="hero-image">{/* <Image /> */}</div>
         <div className="hero-text">
           <p className="text">
+            <h2 className="welcome-text">Welcome</h2>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio
             impedit perspiciatis cumque earum soluta sapiente, qui accusantium
             doloremque! Voluptatibus ad iure ducimus suscipit sapiente dolorum
