f13948204ec5191ecec1d01c0db55257c527cb88
irriz-jayy <jayfelix789@gmail.com>
2023-02-27 23:34:17 +0300

    Added minotr css changes

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,11 +3,16 @@ import Card from "./Card";
 
 const Project = () => {
   return (
-    <div className="projects-section">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div className="projects">
+      <div className="project-header">
+        <h2 className="project-heading">My projects</h2>
+      </div>
+      <div className="projects-section">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
