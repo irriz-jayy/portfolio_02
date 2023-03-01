e72b1286e01c84fbe68bb053d1656a249df34fe8
irriz-jayy <jayfelix789@gmail.com>
2023-03-01 20:07:04 +0300

    Changed language section

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,48 +1,19 @@
 const Languages = () => {
   return (
-    <div className="language-container">
-      <div class="card bg-dark text-white">
-        <img class="card-img" src="https://bit.ly/3J2czHQ" alt="Card image" />
-        <div class="card-img-overlay">
-          <h5 class="card-title">React</h5>
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p class="card-text">Last updated 3 mins ago</p>
+    <div className="language-box">
+      <h1 className="header">Languages</h1>
+      <div className="language-container">
+        <div class="card bg-dark text-white">
+          <img class="card-img" src="https://bit.ly/3y0VIPl" alt="Card image" />
         </div>
-      </div>
-      <div class="card bg-dark text-white">
-        <img class="card-img" src="https://bit.ly/3Y79wTa" alt="Card image" />
-        <div class="card-img-overlay">
-          <h5 class="card-title">HTML</h5>
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p class="card-text">Last updated 3 mins ago</p>
+        <div class="card bg-dark text-white">
+          <img class="card-img" src="https://bit.ly/3Y79wTa" alt="Card image" />
         </div>
-      </div>
-      <div class="card bg-dark text-white">
-        <img class="card-img" src="https://bit.ly/3m8HVnb" alt="Card image" />
-        <div class="card-img-overlay">
-          <h5 class="card-title">Css</h5>
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p class="card-text">Last updated 3 mins ago</p>
+        <div class="card bg-dark text-white">
+          <img class="card-img" src="https://bit.ly/3m8HVnb" alt="Card image" />
         </div>
-      </div>
-      <div class="card bg-dark text-white">
-        <img class="card-img" src="https://bit.ly/3Y9EvOp" alt="Card image" />
-        <div class="card-img-overlay">
-          <h5 class="card-title">JavaScript</h5>
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p class="card-text">Last updated 3 mins ago</p>
+        <div class="card bg-dark text-white">
+          <img class="card-img" src="https://bit.ly/3Y9EvOp" alt="Card image" />
         </div>
       </div>
     </div>
