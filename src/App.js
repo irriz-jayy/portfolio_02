a06a0baa3ef76025e1040d216a6574a0a7fa964d
irriz-jayy <jayfelix789@gmail.com>
2023-04-26 11:22:30 +0300

    Created home section

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import "./App.css";
-import Connect from "./components/Connect";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Languages from "./components/Languages";
-import Navbar from "./components/Navbar";
-import Project from "./components/Project";
+import React from "react";
+import About from "./components/About";
 
 function App() {
-  return (
-    <div className="body">
-      <Navbar />
-      <Home />
-      <Languages />
-      <Project />
-      <Connect />
-      <Footer />
-    </div>
-  );
+  return <About />;
 }
 
 export default App;
